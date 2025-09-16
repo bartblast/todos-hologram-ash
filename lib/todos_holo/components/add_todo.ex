aee6f7b34955f8bfcee3caab2bfadf0adbd29df2
Bart Blast <bart@bartblast.com>
2025-09-16 03:01:28 +0200

    Fix AddTodo component state

--- lib/todos_holo/components/add_todo.ex
+++ lib/todos_holo/components/add_todo.ex
@@ -1,25 +1,22 @@
 defmodule TodosHolo.Components.AddTodo do
   use Hologram.Component
 
-  prop :title, :string, default: ""
-
+  def init(_props, component, _server) do
+    put_state(component, :title, "")
+  end
+  
   def template do
     ~HOLO"""
-    <form $submit="add_todo">
+    <form $submit={command: :add_todo}>
       <input type="text" placeholder="Add a new todo" name="title" value={@title} />
       <button type="submit" class="button">Add</button>
     </form>
     """
   end
 
-  def action(:add_todo, %{event: %{"title" => title}}, component) do
-    component
-    |> put_state(:title, "")    
-    |> put_command(:save_todo, title: title)
-  end
-
-  def command(:save_todo, params, server) do
-    Todos.List.create_todo!(params.title)    
+  def command(:add_todo, %{event: %{"title" => title}}, server) do
+    Todos.List.create_todo!(title)    
+    # server
     put_action(server, name: :reload_page, target: "page")
   end
 end
